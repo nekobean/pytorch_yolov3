cc7635609a8b87ff019b609fd7c1e7dd4fccf914
nekobean <nobepon@gmail.com>
2021-04-21 08:14:46 +0000

    日本語のクラス一覧を追加

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 # Download official YOLOv3 weights trained by MSCOCO.
-wget https://pjreddie.com/media/files/yolov3.weights
+wget -N https://pjreddie.com/media/files/yolov3.weights
 
 # Download official YOLOv3-tiny weights trained by MSCOCO.
-wget https://pjreddie.com/media/files/yolov3-tiny.weights
+wget -N https://pjreddie.com/media/files/yolov3-tiny.weights
